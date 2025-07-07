85a5409e5d69a4d4a5144605cdeb74e24c324356
Katlin2000 <katlin.pala@gmail.com>
2025-07-07 19:57:12 +0100

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by Katlin Pala and is{" "}
+        <a
+          href="https://github.com/Katlin2000/react-weather-app"
+          target="_blank"
+        >
+          open source on Github
+        </a>
+      </footer>
+    </div>
+  );
 }

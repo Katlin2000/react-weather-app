8f528fc41518183b239e91b91da29d0fcf3cc3b4
Katlin2000 <katlin.pala@gmail.com>
2025-07-09 19:53:13 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by Katlin Pala and is{" "}
-        <a
-          href="https://github.com/Katlin2000/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open source on Github
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Katlin Pala and is{" "}
+          <a
+            href="https://github.com/Katlin2000/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

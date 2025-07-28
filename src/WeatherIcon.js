894b86cd37a8f8ca6eea7149a84558729b8db776
Katlin2000 <katlin.pala@gmail.com>
2025-07-28 21:02:12 +0100

    added fahrenheit conversion

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,35 +1,7 @@
 import React from "react";
-import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
-  const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "CLOUDY",
-    "03n": "CLOUDY",
-    "04d": "CLOUDY",
-    "04n": "CLOUDY",
+  const iconUrl = `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${props.code}.png`;
 
-    "09d": "RAIN",
-    "09n": "RAIN",
-    "10d": "RAIN",
-    "10n": "RAIN",
-    "11d": "WIND",
-    "11n": "WIND",
-    "13d": "SNOW",
-    "13n": "SNOW",
-    "50d": "FOG",
-    "50n": "FOG",
-  };
-
-  return (
-    <ReactAnimatedWeather
-      icon={codeMapping[props.code]}
-      color="#1e90ff"
-      size={64}
-      animate={true}
-    />
-  );
+  return <img src={iconUrl} alt="Weather icon" width={64} height={64} />;
 }

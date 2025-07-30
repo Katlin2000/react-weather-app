9899ad558add067d1ece6c55774ecac86e1a0899
Katlin2000 <katlin.pala@gmail.com>
2025-07-30 19:52:28 +0100

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-  const days = [
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -11,12 +11,12 @@ export default function FormattedDate(props) {
     "Saturday",
   ];
 
-  const day = days[props.date.getDay()];
-  const hours = props.date.getHours();
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  const minutes = props.date.getMinutes();
+  let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
